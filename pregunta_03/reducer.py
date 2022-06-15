3ce5fc83e034b69200ff84ce784bc0bee39cda6f
Dimorenoa <101435071+Dimorenoa@users.noreply.github.com>
2022-06-14 21:00:10 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,18 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    lista1 = []
+    lista2 = []
+   
+    for line in sys.stdin:
+        lista1.append(line)
+
+  
+    lista2 = sorted(lista1, key=lambda x: x[2])
+
+    for item in lista2:
+        sys.stdout.write("{}".format(item))
